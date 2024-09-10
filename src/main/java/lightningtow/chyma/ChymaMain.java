c0001b7fe1b354c02962a0d74e4ecb8c3ea85662
Lightningtow <alexwiest01@gmail.com>
2024-09-09 17:53:39 -0700

    fixed it

--- src/main/java/lightningtow/chyma/ChymaMain.java
+++ src/main/java/lightningtow/chyma/ChymaMain.java
@@ -1,24 +1,41 @@
-//package lightningtow.chyma;
-//
-//import net.fabricmc.api.ClientModInitializer;
-//
-//public class ChymaMain implements ClientModInitializer
-//{
-//
-//
-//
-//	@Override
-//	public void onInitializeClient() {
-//		// journeymap initialization happens in JourneymapIntegration. It is not referenced elsewhere to leave it as a soft dependency
-//
-////		try {
-////			CustomhudIntegration.initCustomhud();
-////			LogThis(Level.INFO, "Successfully integrated with CustomHud");
-////
-////		} catch (Exception e) {
-////			LogThis(Level.ERROR, "Error integrating with CustomHud: " + e);
-////		}
-//
-//	}
-//
-//}
+package lightningtow.chyma;
+
+import com.minenash.customhud.HudElements.interfaces.HudElement;
+import com.minenash.customhud.HudElements.supplier.BooleanSupplierElement;
+import com.minenash.customhud.data.Flags;
+import com.minenash.customhud.registry.CustomHudRegistry;
+import net.fabricmc.api.ClientModInitializer;
+import org.apache.logging.log4j.Level;
+
+public class ChymaMain implements ClientModInitializer
+{
+	public static final String MOD_ID = "chyma";
+	public static final String MOD_DISPLAY_NAME = "Chyma";
+
+	public static void LogThis(String msg) { LogThis(org.apache.logging.log4j.Level.INFO, msg); } // 'default' parameter cause java is dumb
+	public static void LogThis(org.apache.logging.log4j.Level lvl, String msg) {
+		msg = "(" + MOD_DISPLAY_NAME + ") " + msg;
+		org.apache.logging.log4j.LogManager.getLogger(MOD_DISPLAY_NAME).log(lvl, msg); }
+
+
+	public static boolean mapVisible = true;
+
+
+	@Override
+	public void onInitializeClient() {
+		// journeymap initialization happens in JourneymapIntegration. It is not referenced elsewhere to leave it as a soft dependency
+		try {
+
+			HudElement elem = new BooleanSupplierElement(() -> (ChymaMain.mapVisible));
+
+			CustomHudRegistry.registerElement("jm", (f, c) -> Flags.wrap(elem, f));
+			CustomHudRegistry.registerElement("jm_map", (f, c) -> Flags.wrap(elem, f));
+			CustomHudRegistry.registerElement("jm_minimap", (f, c) -> Flags.wrap(elem, f));
+			LogThis("Successfully registered Journeymap variables with CustomHud");
+		}
+		catch (Exception e) { LogThis(Level.ERROR, "Could not register Journeymap variables with CustomHud: " + e.getMessage()); }
+
+
+	}
+
+}
